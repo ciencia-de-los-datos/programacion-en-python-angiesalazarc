bc1c1bb454f50ccdf84f933ac7bf88b900714340
angiesalazarc <101910520+angiesalazarc@users.noreply.github.com>
2022-04-02 20:23:30 -0500

    Solución pregunta 2

--- preguntas.py
+++ preguntas.py
@@ -37,20 +37,29 @@ def pregunta_01():
 
 def pregunta_02():
     """
-    Retorne la cantidad de registros por cada letra de la primera columna como la lista
-    de tuplas (letra, cantidad), ordendas alfabéticamente.
-
-    Rta/
-    [
-        ("A", 8),
-        ("B", 7),
-        ("C", 5),
-        ("D", 6),
-        ("E", 14),
-    ]
-
+        2Retorne la cantidad de registros por cada letra de la primera columna como la lista
+        de tuplas (letra, cantidad), ordendas alfabéticamente.
+        Rta/
+        [
+            ("A", 8),
+            ("B", 7),
+            ("C", 5),
+            ("D", 6),
+            ("E", 14),
+        ]
     """
-    return
+
+    dic = {}
+    for letter in colums:
+        if letter[0] in dic.keys():
+            dic[letter[0]] = dic[letter[0]] +1 
+        else:
+            dic[letter[0]] = 1
+
+    tupla = list(zip(dic.keys(), dic.values()))
+    tupla.sort()
+
+    return tupla
 
 
 def pregunta_03():
